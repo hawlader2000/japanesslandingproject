de550a23539b9c9b48be1712ac7ff6ccf971312f
hawlader44c <mdbadolhawlader44c@gmail.com>
2023-01-12 19:21:59 +0600

    Japanees Big Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./styles/styles.module.css";
+import Image from "react-bootstrap/Image";
+import logo from "./images/logo.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-xl">
+        <div className="container">
+          <div className="d-flex flex-row">
+            <div>
+              <h3>RECRUIT DIRECT SCOUT</h3>
+              <p>リクルートダイレクトスカウト</p>
+            </div>
+            <div>
+              <button id={styles.headerbtn}>ログイン</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section>
+        <div id={styles.backgroundimg}>
+          <div id={styles.sec1txt}>
+            <p id={styles.sextxt1}>年収800万~2,000万円の</p>
+            <p id={styles.sectxt1}>ハイクラス転職</p>
+            <Image src={logo} alt="logo" />
+          </div>
+          <div id={styles.txtgroup} className="text-white">
+            <p id={styles.txtgrouptxt1}>転職後の平均年収</p>
+            <div className="row">
+              <p id={styles.txtgrouptxt2} className="col-6">
+                938
+              </p>
+              <p id={styles.txtgrouptxt3} className="col-6">
+                万円
+              </p>
+            </div>
+            <p>※2022 年 4 月時点、ハイキャリア会員のみ</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
